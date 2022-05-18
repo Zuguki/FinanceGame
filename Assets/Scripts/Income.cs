382f379a183ec05fa7e53f83afe0635316532777
Zuguki <guskov.zahar20@gmail.com>
2022-05-18 19:12:48 +0500

    Time for income was added

--- Assets/Scripts/Income.cs
+++ Assets/Scripts/Income.cs
@@ -4,11 +4,14 @@
     {
         public int Title { get; }
         public int Value { get; }
+        
+        public int Time { get; }
 
-        public Income(int title, int value)
+        public Income(int title, int value, int time)
         {
             Title = title;
             Value = value;
+            Time = time;
         }
     }
 }
