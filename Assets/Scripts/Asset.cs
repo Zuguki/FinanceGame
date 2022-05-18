8782e5623029f7b081b25dbf7bbb7e96ddea86b0
Zuguki <guskov.zahar20@gmail.com>
2022-05-18 19:11:27 +0500

    Time for assets was added

--- Assets/Scripts/Asset.cs
+++ Assets/Scripts/Asset.cs
@@ -5,12 +5,15 @@
         public string Title { get; }
         public int Price { get; }
         public int IncomeValue { get; }
+        
+        public int NeedsTime { get; }
 
-        public Asset(string title, int price, int incomeValue)
+        public Asset(string title, int price, int incomeValue, int needsTime)
         {
             Title = title;
             Price = price;
             IncomeValue = incomeValue;
+            NeedsTime = needsTime;
         }
     }
 }
