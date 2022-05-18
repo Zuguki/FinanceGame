05fac3c47711b3d4f4d2ed7bde30e3409fdf1dda
Zuguki <guskov.zahar20@gmail.com>
2022-05-18 19:08:46 +0500

    Asset was added

---
+++ Assets/Scripts/Asset.cs
@@ -0,0 +1,16 @@
+﻿namespace DefaultNamespace
+{
+    public class Asset
+    {
+        public string Title { get; }
+        public int Price { get; }
+        public int IncomeValue { get; }
+
+        public Asset(string title, int price, int incomeValue)
+        {
+            Title = title;
+            Price = price;
+            IncomeValue = incomeValue;
+        }
+    }
+}
