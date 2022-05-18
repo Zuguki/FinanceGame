cb2e6763e2bf3216a6e4c8b522cef218ec9db71a
Zuguki <guskov.zahar20@gmail.com>
2022-05-18 18:38:27 +0500

    Passive was added

---
+++ Assets/Scripts/Passive.cs
@@ -0,0 +1,16 @@
+﻿namespace DefaultNamespace
+{
+    public class Passive
+    {
+        public int MoneyForMonth { get; private set; }
+        public int TimeForMonth { get; private set; }
+        public int MonthLength { get; set; }
+
+        public Passive(int moneyForMonth, int timeForMonth, int monthLength)
+        {
+            MoneyForMonth = moneyForMonth;
+            TimeForMonth = timeForMonth;
+            MonthLength = monthLength;
+        }
+    }
+}
