4fc592dae62293608486fe97c32066e4d7ed2f0f
Zuguki <guskov.zahar20@gmail.com>
2022-05-18 19:34:33 +0500

    PlayerScript was added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Linq;
+using DefaultNamespace;
+using TMPro;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static bool NeedsUpdate = true;
+    
+    [SerializeField] private GameObject playerInfoUI;
+    
+    private const int TimePerMonth = 100;
+    public static int Cash, Mood;
+    public static List<Income> Incomes = new();
+    public static List<Expense> Expenses = new();
+    public static List<Passive> Liabilities = new();
+    public static List<Asset> Assets = new();
+    
+    public static int CashFlow => Incomes.Sum(inc => inc.Value) + Assets.Sum(asset => asset.Value) 
+                                  - (Expenses.Sum(exp => exp.Value) 
+                                     + Liabilities.Sum(pas => pas.Value));
+
+    public static int FreeTime => TimePerMonth - (Incomes.Sum(inc => inc.Time)
+                                               + Expenses.Sum(exp => exp.Time)
+                                               + Liabilities.Sum(pas => pas.Time) +
+                                               Assets.Sum(asset => asset.Time));
+
+    private TextMeshProUGUI _cashText, _cashFlowText, _incomeText, _expensesText, _assetsText, _liabilitiesText,
+        _freeTimeText, _moodText;
+
+    private void Awake()
+    {
+        _cashText = playerInfoUI.transform.GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>();
+        _cashFlowText = playerInfoUI.transform.GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        _incomeText = playerInfoUI.transform.GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        _expensesText = playerInfoUI.transform.GetChild(4).GetChild(0).GetComponent<TextMeshProUGUI>();
+        _assetsText = playerInfoUI.transform.GetChild(5).GetChild(0).GetComponent<TextMeshProUGUI>();
+        _liabilitiesText = playerInfoUI.transform.GetChild(6).GetChild(0).GetComponent<TextMeshProUGUI>();
+        _freeTimeText = playerInfoUI.transform.GetChild(7).GetChild(0).GetComponent<TextMeshProUGUI>();
+        _moodText = playerInfoUI.transform.GetChild(8).GetChild(0).GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        if (NeedsUpdate)
+            UpdateUIValues();
+    }
+
+    private void UpdateUIValues()
+    {
+        _cashText.text = Cash.ToString();
+        _cashFlowText.text = CashFlow.ToString();
+        _incomeText.text = Incomes.Sum(inc => inc.Value).ToString();
+        _expensesText.text = Expenses.Sum(exp => exp.Value).ToString();
+        _assetsText.text = Assets.Sum(asset => asset.Price).ToString();
+        _liabilitiesText.text = Liabilities.Sum(pas => pas.Value).ToString();
+        _freeTimeText.text = FreeTime.ToString();
+        _moodText.text = Mood.ToString();
+
+        NeedsUpdate = false;
+    }
+}
