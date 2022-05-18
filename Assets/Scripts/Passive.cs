066b89ce3ce725aacdd96ea87df8f20dcc222a4f
Zuguki <guskov.zahar20@gmail.com>
2022-05-18 19:06:41 +0500

    Passive was updated

--- Assets/Scripts/Passive.cs
+++ Assets/Scripts/Passive.cs
@@ -2,12 +2,14 @@
 {
     public class Passive
     {
+        public string Title { get; }
         public int MoneyForMonth { get; private set; }
         public int TimeForMonth { get; private set; }
         public int MonthLength { get; set; }
 
-        public Passive(int moneyForMonth, int timeForMonth, int monthLength)
+        public Passive(string title, int moneyForMonth, int timeForMonth, int monthLength)
         {
+            Title = title;
             MoneyForMonth = moneyForMonth;
             TimeForMonth = timeForMonth;
             MonthLength = monthLength;
