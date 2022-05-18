11fa8cefb167461e93c1d2b5f8fc2d78a41adf14
Zuguki <guskov.zahar20@gmail.com>
2022-05-18 18:49:00 +0500

    Income was added

---
+++ Assets/Scripts/Income.cs
@@ -0,0 +1,14 @@
+﻿namespace DefaultNamespace
+{
+    public class Income
+    {
+        public int IncomeTitle { get; }
+        public int IncomeValue { get; }
+
+        public Income(int incomeTitle, int incomeValue)
+        {
+            IncomeTitle = incomeTitle;
+            IncomeValue = incomeValue;
+        }
+    }
+}
