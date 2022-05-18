76262feef99bd428335e8a580384ea0d352eaf85
Zuguki <guskov.zahar20@gmail.com>
2022-05-18 19:14:04 +0500

    Time for Expense was added

--- Assets/Scripts/Expense.cs
+++ Assets/Scripts/Expense.cs
@@ -4,11 +4,13 @@
     {
         public string Title { get; }
         public int Value { get; }
+        public int Time { get; }
 
-        public Expense(string title, int value)
+        public Expense(string title, int value, int time)
         {
             Title = title;
             Value = value;
+            Time = time;
         }
     }
 }
