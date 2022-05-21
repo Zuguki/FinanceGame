afc960e30ba185a36731ebfdbe4fff1c8264db56
Zuguki <guskov.zahar20@gmail.com>
2022-05-21 16:01:53 +0500

    DH done

---
+++ Assets/Scripts/Health/DefaultHealth.cs
@@ -0,0 +1,11 @@
+﻿namespace DefaultNamespace
+{
+    public class DefaultHealth : IHealthInfo
+    {
+        public string Title() => "Вы здоровы";
+
+        public string Details(int price) => "У вас все хорошо со здоросьем";
+
+        public int Time() => 0;
+    }
+}
