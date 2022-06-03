5d421923077d20d5d11bb2f188e5ac69e4316413
Zuguki <guskov.zahar20@gmail.com>
2022-06-03 21:47:18 +0500

    Pivovarnya was added

---
+++ Assets/Scripts/Assets/Business/Pivovarnya.cs
@@ -0,0 +1,16 @@
+﻿namespace DefaultNamespace
+{
+    public class Pivovarnya : IBusinessInfo
+    {
+        public string Title => "Пивоварня";
+
+        public string Details => $"Вы можете приобрести пивоварню за: {Price}р и получать за нее {Income}р/мес\n\n" +
+                                 $"Требуется: \n\n" +
+                                 $"{Price}р\n{NeedsTime} часов/мес";
+
+        public int Price => 100_000;
+        public int NeedsTime => 25;
+        public int Income => 50_000;
+        public Business BusinessInfo => Business.Low;
+    }
+}
