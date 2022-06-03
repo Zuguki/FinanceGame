c1a096d8f3673e66032f436b4018f0437574bedc
Zuguki <guskov.zahar20@gmail.com>
2022-06-03 18:30:20 +0500

    Assets UI picture

--- Assets/Scripts/AssetsCell.cs
+++ Assets/Scripts/AssetsCell.cs
@@ -12,12 +12,10 @@ namespace DefaultNamespace
 
         private Button _businessButton;
         private Button _realtyButton;
-        private Button _stocksButton;
         private Button _cancelButton;
 
         private TextMeshProUGUI _business;
         private TextMeshProUGUI _realty;
-        private TextMeshProUGUI _stocks;
         private TextMeshProUGUI _cancel;
         
         private void Awake()
@@ -26,12 +24,10 @@ namespace DefaultNamespace
 
             _businessButton = cellUI.transform.GetChild(1).GetComponent<Button>();
             _realtyButton = cellUI.transform.GetChild(2).GetComponent<Button>();
-            _stocksButton = cellUI.transform.GetChild(3).GetComponent<Button>();
             _cancelButton = cellUI.transform.GetChild(4).GetComponent<Button>();
 
             _business = _businessButton.GetComponentInChildren<TextMeshProUGUI>();
             _realty = _realtyButton.GetComponentInChildren<TextMeshProUGUI>();
-            _stocks = _stocksButton.GetComponentInChildren<TextMeshProUGUI>();
             _cancel = _cancelButton.GetComponentInChildren<TextMeshProUGUI>();
             
             _cancelButton.onClick.AddListener(Success);
