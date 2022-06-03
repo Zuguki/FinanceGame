c1e6744f318f55bedc69dd486e64bf96e5c873c1
Zuguki <guskov.zahar20@gmail.com>
2022-06-04 00:14:07 +0500

    Home was added

---
+++ Assets/Scripts/Assets/Realty/Home.cs
@@ -0,0 +1,12 @@
+﻿namespace DefaultNamespace
+{
+    public class Home : IRealtyInfo
+    {
+        public string Title => "Загородный дом";
+        public string Details => $"Стоимость покупки недвижимости: {Price}\nПрибыль: {Income}";
+        public int Price => 500_000;
+        public int NeedsTime => 0;
+        public int Income => 15_000;
+        public Realty RealtyInfo => Realty.Low;
+    }
+}
