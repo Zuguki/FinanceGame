aa8c989bfa6e80a2f2a9d435aa74f42031bb5aae
Zuguki <guskov.zahar20@gmail.com>
2022-06-03 21:38:10 +0500

    BusinessInfo done

---
+++ Assets/Scripts/Assets/Business/IBusinessInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace DefaultNamespace
+{
+    public interface IBusinessInfo
+    {
+        public string Title { get; }
+        
+        public string Details { get; }
+        
+        public int Price { get; }
+        
+        public int NeedsTime { get; }
+        
+        public int Income { get; }
+        
+        public Business BusinessInfo { get; }
+    }
+}
