7ebd2875dac748f08f63c3b588f4787183a8d375
Zuguki <guskov.zahar20@gmail.com>
2022-06-03 21:38:24 +0500

    RealtyInfo done

---
+++ Assets/Scripts/Assets/Realty/IRealtyInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace DefaultNamespace
+{
+    public interface IRealtyInfo
+    {
+        public string Title { get; }
+        
+        public string Details { get; }
+        
+        public int Price { get; }
+        
+        public int NeedsTime { get; }
+        
+        public int Income { get; }
+        
+        public Realty RealtyInfo { get; }
+    }
+}
---
+++ Assets/Scripts/Assets/Realty/Realty.cs
@@ -0,0 +1,9 @@
+﻿namespace DefaultNamespace
+{
+    public enum Realty
+    {
+        Low,
+        Middle,
+        Height
+    }
+}
