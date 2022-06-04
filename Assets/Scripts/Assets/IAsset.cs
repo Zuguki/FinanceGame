795f5fd11562d6c11047be1a30dc1ebdf1a50b06
Zuguki <guskov.zahar20@gmail.com>
2022-06-04 10:37:05 +0500

    IAsset was added

---
+++ Assets/Scripts/Assets/IAsset.cs
@@ -0,0 +1,19 @@
+﻿namespace DefaultNamespace
+{
+    public interface IAsset
+    {
+        public string Title { get; }
+        
+        public string Details { get; }
+        
+        public int Price { get; }
+        
+        public int Income { get; }
+        
+        public int NeedsTime { get; }
+        
+        public Business BusinessInfo { get; }
+        
+        public Realty RealtyInfo { get; }
+    }
+}
