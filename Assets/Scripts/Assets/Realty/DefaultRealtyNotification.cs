c7fe66aedaac9d7bab0e468b732c48925050046d
Zuguki <guskov.zahar20@gmail.com>
2022-06-04 10:37:26 +0500

    Default for Realty was added

---
+++ Assets/Scripts/Assets/Realty/DefaultRealtyNotification.cs
@@ -0,0 +1,13 @@
+﻿namespace DefaultNamespace
+{
+    public class DefaultRealtyNotification : IAsset
+    {
+        public string Title => "Извините";
+        public string Details => "К сожалению, в этой категории больше нет недвижимости для вас";
+        public int Price { get; }
+        public int NeedsTime { get; }
+        public Business BusinessInfo { get; }
+        public int Income { get; }
+        public Realty RealtyInfo { get; }
+    }
+}
