4ad3f09b71f67a8f911a4607b3a602293f34c670
Zuguki <guskov.zahar20@gmail.com>
2022-06-04 10:37:42 +0500

    Default for Business was added

---
+++ Assets/Scripts/Assets/Business/DefaultBusinessNotification.cs
@@ -0,0 +1,13 @@
+﻿namespace DefaultNamespace
+{
+    public class DefaultBusinessNotification : IAsset
+    {
+        public string Title => "Изнините";
+        public string Details => "К сожалению в этой категории больше нет бизнесов для вас";
+        public int Price { get; }
+        public int NeedsTime { get; }
+        public int Income { get; }
+        public Business BusinessInfo { get; }
+        public Realty RealtyInfo { get; }
+    }
+}
