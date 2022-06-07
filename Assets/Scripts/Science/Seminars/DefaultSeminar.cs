d779b3b1d43c433aa3bc191c0fb3ce5b3577b1f1
Zuguki <guskov.zahar20@gmail.com>
2022-06-07 18:12:09 +0500

    Expiration Date was updated

--- Assets/Scripts/Science/Seminars/DefaultSeminar.cs
+++ Assets/Scripts/Science/Seminars/DefaultSeminar.cs
@@ -5,7 +5,7 @@
         public string Title => "Нету";
         public string Description => "Поздравляем, вы прошли все семинары";
         public int Price => 0;
-        public int ExpirationDate => 0;
+        public int ExpirationDate => 1;
         public int NeedsTime => 0;
         public StudyTrack Track => StudyTrack.Seminar;
     }
