e979f0fe504e60726616d7c92d90ccc6405b29d0
Zuguki <guskov.zahar20@gmail.com>
2022-06-07 18:11:43 +0500

    Default HE was added

---
+++ Assets/Scripts/Science/HeightEducation/DefaultHeightEducation.cs
@@ -0,0 +1,12 @@
+﻿namespace DefaultNamespace.HeightEducation
+{
+    public class DefaultHeightEducation : IStudyCell
+    {
+        public string Title => "Поздравляю";
+        public string Description => "Поздравляем, вы получили всевозможное высшее образование";
+        public int Price => 0;
+        public int ExpirationDate => 1;
+        public int NeedsTime => 0;
+        public StudyTrack Track => StudyTrack.HigherEducation;
+    }
+}
