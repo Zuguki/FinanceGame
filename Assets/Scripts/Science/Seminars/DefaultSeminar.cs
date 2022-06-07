0e09a1a8eb990fbc37d44e1f7da308ec5961a370
Zuguki <guskov.zahar20@gmail.com>
2022-06-07 18:09:46 +0500

    Default seminar was added

---
+++ Assets/Scripts/Science/Seminars/DefaultSeminar.cs
@@ -0,0 +1,12 @@
+﻿namespace DefaultNamespace.Seminars
+{
+    public class DefaultSeminar : IStudyCell
+    {
+        public string Title => "Нету";
+        public string Description => "Поздравляем, вы прошли все семинары";
+        public int Price => 0;
+        public int ExpirationDate => 0;
+        public int NeedsTime => 0;
+        public StudyTrack Track => StudyTrack.Seminar;
+    }
+}
