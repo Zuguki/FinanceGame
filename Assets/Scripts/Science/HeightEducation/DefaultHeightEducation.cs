5e1bfacf73d8ca101d3efd7dea8f069fe36affc0
Zuguki <guskov.zahar20@gmail.com>
2022-06-08 08:55:15 +0500

    Work with Education

--- Assets/Scripts/Science/HeightEducation/DefaultHeightEducation.cs
+++ Assets/Scripts/Science/HeightEducation/DefaultHeightEducation.cs
@@ -7,6 +7,7 @@
         public int Price => 0;
         public int ExpirationDate => 1;
         public int NeedsTime => 0;
+        public int RatioOfUpgrade => 0;
         public StudyTrack Track => StudyTrack.HigherEducation;
     }
 }
