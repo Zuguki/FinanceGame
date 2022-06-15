3c9d6b5deab5cb54ac4276c246bd3195533fde3b
Zuguki <guskov.zahar20@gmail.com>
2022-06-15 16:08:40 +0500

    Converter done

--- Assets/Scripts/Converter.cs
+++ Assets/Scripts/Converter.cs
@@ -1,7 +1,37 @@
-﻿namespace DefaultNamespace
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Main
 {
-    public class Converter
+    public static class Converter
     {
-        
+        public static string ConvertToString(string value)
+        {
+            var counter = 0;
+            var sb = new StringBuilder();
+            for (var index = value.Length - 1; index >= 0; index--)
+            {
+                sb.Append(value[index]);
+                counter++;
+                
+                if (counter < 3)
+                    continue;
+
+                counter = 0;
+                sb.Append(".");
+            }
+
+            var array = sb.ToString().ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+
+        public static int ConvertToInt(string value)
+        {
+            var clearString = new string(value.Where(item => item != '.').ToArray());
+            return int.Parse(clearString);
+        }
     }
+
 }
