a44aa6351d97a5ed7178f3c6333915e89c04bbe7
Zuguki <guskov.zahar20@gmail.com>
2022-06-17 02:12:55 +0500

    Targets was created;

---
+++ Assets/Scripts/Targets/ITarget.cs
@@ -0,0 +1,11 @@
+﻿namespace Targets
+{
+    public interface ITarget
+    {
+        public int YearsTime { get; }
+        public int BusinessCount { get; }
+        public int RealtyCount { get; }
+        public int TimesFlow { get; }
+        public int MoodStat { get; }
+    }
+}
